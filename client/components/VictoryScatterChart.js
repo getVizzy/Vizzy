b090c8cd197081fffdbe0a5be10d5484a538f297
rumroses12 <grace@daher.net>
2018-11-18 18:55:28 -0500

    Feature(added regression line option to scatterplot)

--- client/components/VictoryScatterChart.js
+++ client/components/VictoryScatterChart.js
@@ -14,14 +14,20 @@ import {
   VictoryLabel,
   VictoryVoronoiContainer
 } from 'victory'
+import * as tf from '@tensorflow/tfjs'
 
 const data = [
   {'Degrees Celsius Below Zero': 1, 'Hundreds of Pounds of Cocoa Sold': 2},
-  {'Degrees Celsius Below Zero': 2, 'Hundreds of Pounds of Cocoa Sold': 3},
-  {'Degrees Celsius Below Zero': 3, 'Hundreds of Pounds of Cocoa Sold': 5},
-  {'Degrees Celsius Below Zero': 4, 'Hundreds of Pounds of Cocoa Sold': 4},
-  {'Degrees Celsius Below Zero': 5, 'Hundreds of Pounds of Cocoa Sold': 7}
+  {'Degrees Celsius Below Zero': 2, 'Hundreds of Pounds of Cocoa Sold': 2},
+  {'Degrees Celsius Below Zero': 3, 'Hundreds of Pounds of Cocoa Sold': 4},
+  {'Degrees Celsius Below Zero': 4, 'Hundreds of Pounds of Cocoa Sold': 3},
+  {'Degrees Celsius Below Zero': 5, 'Hundreds of Pounds of Cocoa Sold': 4.5},
+  {'Degrees Celsius Below Zero': 6, 'Hundreds of Pounds of Cocoa Sold': 4.5},
+  {'Degrees Celsius Below Zero': 7, 'Hundreds of Pounds of Cocoa Sold': 7},
+  {'Degrees Celsius Below Zero': 8, 'Hundreds of Pounds of Cocoa Sold': 10}
 ]
+const xs = tf.tensor1d([1, 2, 3, 4, 5, 6, 7, 8])
+const ys = tf.tensor1d([2, 2, 4, 3, 4.5, 4.5, 7, 10])
 
 class VictoryScatterChart extends Component {
   constructor() {
@@ -32,9 +38,12 @@ class VictoryScatterChart extends Component {
       y: null,
       title: '',
       highlight: 'orange',
-      tooltip: '5'
+      tooltip: '5',
+      regression: false,
+      regressionLine: []
     }
     this.changeStyle = this.changeStyle.bind(this)
+    this.buildRegressionModel = this.buildRegressionModel.bind(this)
   }
 
   handleZoom(domain) {
@@ -47,6 +56,60 @@ class VictoryScatterChart extends Component {
     })
   }
 
+  buildRegressionModel(xCol, yCol) {
+    //there are different tensor types for different tensor dimensions
+    const yData = data.map(elem => elem[xCol])
+    const xData = data.map(elem => elem[yCol])
+    console.log('xData', xData, 'yData', yData)
+    const ys = tf.tensor1d(yData)
+    const xs = tf.tensor1d(xData)
+
+    //in tensorflow, variables can be mutated. We will be changing the values of these variables when we run the optimizer. Here we make a scalar and turn it into a variable so we can change it.
+    let m = tf.scalar(Math.random()).variable()
+    let b = tf.scalar(Math.random()).variable()
+
+    //define your model, y=mx + b
+    //The tensorflow library provides mathematical operation methods to tensors that facilitate things like multiplication or addition of tensors.
+    const model = x => m.mul(x).add(b)
+
+    //determine how quickly to adjust the model
+    const learningRate = 0.01
+    const optimizer = tf.train.sgd(learningRate)
+
+    //pred are the y values predicted by your model.
+    //actual is the actual y values in the dataset.
+    const loss = (pred, actual) =>
+      pred
+        .sub(actual)
+        .square()
+        .mean()
+    //here, we train the model. With each loop, the model is adjusted to minimize the output of our loss function.
+
+    for (let i = 0; i < 10; ++i) {
+      optimizer.minimize(() => loss(model(xs), ys))
+    }
+
+    const modelPredictedValues = model(xs).dataSync()
+
+    //the datasync method returns the tensor data in the form of an array. We access it with using this dataSync method.
+    const x = xs.dataSync()
+    const y = ys.dataSync()
+
+    //start and end points of regression line
+    const x1 = x[0]
+    const y_pred_1 = modelPredictedValues[0]
+    const x2 = x[x.length - 1]
+    const y_pred_2 = modelPredictedValues[x.length - 1]
+    const regressionLine = [
+      {[xCol]: x1, [yCol]: y_pred_1},
+      {[xCol]: x2, [yCol]: y_pred_2}
+    ]
+    console.log(regressionLine, 'regressionLine')
+    this.setState({
+      regressionLine: regressionLine
+    })
+  }
+
   render() {
     if (!data) {
       return 'Loading...'
@@ -54,6 +117,7 @@ class VictoryScatterChart extends Component {
       let keys = Object.keys(data[0])
       let y = this.state.y || keys[1]
       let x = this.state.x || keys[0]
+
       return (
         <div id="container">
           <div id="chart">
@@ -129,6 +193,28 @@ class VictoryScatterChart extends Component {
                   }
                 ]}
               />
+              <VictoryLine
+                data={this.state.regressionLine}
+                x={x}
+                y={y}
+                animate={{
+                  duration: 2000,
+                  onLoad: {duration: 1000}
+                }}
+              />
+              <VictoryLabel
+                text={this.state.title}
+                style={{
+                  fontSize: 16,
+                  textAnchor: 'start',
+                  verticalAnchor: 'end',
+                  fill: '#000000',
+                  fontFamily: 'inherit',
+                  fontWeight: 'bold'
+                }}
+                x={100}
+                y={24}
+              />
 
               <VictoryAxis
                 label={x}
@@ -174,7 +260,6 @@ class VictoryScatterChart extends Component {
                 value={keys[0]}
               >{`${keys[0][0].toUpperCase()}${keys[0].slice(1)}`}</option>
             </select>
-
             <p>Dot Color:</p>
             <select onChange={e => this.changeStyle(e.target.value, 'color')}>
               <option value="tomato">Tomato</option>
@@ -184,19 +269,31 @@ class VictoryScatterChart extends Component {
               <option value="#55e">Purple</option>
               <option value="#8af">Periwinkle</option>
             </select>
-
             <p>Pointer:</p>
             <select onChange={e => this.changeStyle(e.target.value, 'tooltip')}>
               <option value={5}>Round edge</option>
               <option value={0}>Square</option>
               <option value={25}>Circle</option>
             </select>
-
             <p>Graph Title:</p>
             <input
               value={this.state.title}
               onChange={e => this.changeStyle(e.target.value, 'title')}
             />
+            <p>
+              Regression Line:{' '}
+              <input
+                type={'checkbox'}
+                onChange={async e => {
+                  await this.changeStyle(!this.state.regression, 'regression')
+                  if (this.state.regression) {
+                    this.buildRegressionModel(x, y)
+                  } else {
+                    this.changeStyle([], 'regressionLine')
+                  }
+                }}
+              />
+            </p>
           </div>
         </div>
       )
