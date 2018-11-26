38f7d3422a13fae700d9b440df114a06d3eab784
rumroses12 <grace@daher.net>
2018-11-26 13:43:40 -0500

    Feature(wrote backend post route to send emails)

--- server/api/room.js
+++ server/api/room.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const Room = require('../db/models/room')
-module.exports = router
+const nodemailer = require('nodemailer')
 
+module.exports = router
 
 // POST api/room - Add room
 router.post('/', async (req, res, next) => {
@@ -9,8 +10,9 @@ router.post('/', async (req, res, next) => {
     let room = await Room.create(req.body)
 
     res.status(201).json(room)
+  } catch (error) {
+    next(error)
   }
-  catch (error) { next(error) }
 })
 
 // GET api/room  - get ALL rooms
@@ -18,9 +20,37 @@ router.get('/', async (req, res, next) => {
   try {
     let rooms = await Room.findAll()
     res.json(rooms)
+  } catch (error) {
+    next(error)
   }
-  catch (error) { next(error) }
 })
 
+// POST api/room/email - sends an email to user with the room id
 
+router.post('/email', (req, res, next) => {
+  var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'vizzynoreply@gmail.com',
+      pass: 'H&llo123'
+    }
+  })
+  // const mailOptions = {
+  //   from: 'vizzynoreply@gmail.com', // sender address
+  //   to: 'grace@daher.net', // list of receivers
+  //   subject: 'Subject of your email', // Subject line
+  //   html: '<p>Your html here</p>' // plain text body
+  //}
+  const mailOptions = {}
+  mailOptions.from = 'vizzynoreply@gmail.com'
+  mailOptions.to = req.body.to
+  mailOptions.subject = req.body.subject
+  mailOptions.html = req.body.html
 
+  transporter.sendMail(mailOptions, function(err, info) {
+    if (err) console.log(err)
+    else console.log(info)
+  })
+
+  res.send()
+})
