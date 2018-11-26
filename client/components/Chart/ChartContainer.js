fc955f06aefb868ca07d2480c0113ad32e5cf297
Adrienne Johnson <adriennetjohnson@gmail.com>
2018-11-26 00:04:11 -0500

    Fix(Added Label for Pie Title - Not Formatted Completely)

--- client/components/Chart/ChartContainer.js
+++ client/components/Chart/ChartContainer.js
@@ -3,6 +3,7 @@ import Bar from './VictoryBarChart'
 import Line from './VictoryLineGraph'
 import Scatter from './VictoryScatterChart'
 import Pie from './VictoryPieChart'
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 export default class ChartContainer extends Component {
@@ -16,7 +17,10 @@ export default class ChartContainer extends Component {
 
   render() {
     if(!Object.keys(this.props.data[0]).includes(this.props.x)) {
-      return "Gathering data..."
+      return <div id="working">
+        <p>Graph in progress...</p>
+        <CircularProgress/>
+      </div>
     }
 
     return this.props.graphSelected === 'bar' ? (
