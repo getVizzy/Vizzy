1e9e0221dffddbfb2ef689e3e75ed7136f101463
Adrienne Johnson <adriennetjohnson@gmail.com>
2018-11-30 12:38:22 -0500

    Fix(Delete Graphs with Dataset)

--- server/api/data.js
+++ server/api/data.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const {User, Data} = require('../db/models')
+const {User, Data, Graph} = require('../db/models')
 
 router.post('/', async (req, res, next) => {
   try {
@@ -33,11 +33,18 @@ router.get('/', async (req, res, next) => {
 
 router.delete('/:dataId', async (req, res, next) => {
   try {
+    await Graph.destroy({
+      where: {
+        datumId: req.params.dataId
+      }
+    })
+
     const dataSet = await Data.destroy({
       where: {
         id: req.params.dataId
       }
     })
+
     res.sendStatus(202)
   } catch (error) {
     next(error)
